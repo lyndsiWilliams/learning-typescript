5d5e84db2d09ffdf9ff3450cc9677fec86a30c8d
lyndsiWilliams <kcatgirl@gmail.com>
2021-01-20 14:19:23 -0600

    Lesson 10 complete

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -168,4 +168,35 @@ const logDetails = (uid: stringOrNum, item: string) => {
 const greet = (user: objWithName) => {
   console.log(`${user.name} says hello`);
 };
+
+
+// ---------- Function Signatures ----------
+// Basic function type -- let greet: Function;
+// Advances function type:
+
+// Example 1
+let greet: (a: string, b: string) => void;
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+// Example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add'){
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  };
+};
+
+// Example 3
+type person = { name: string, age: number }
+
+let logDetails: (obj: person) => void;
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+};
 */
