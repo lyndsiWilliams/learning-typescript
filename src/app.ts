82fa8e04d3501eef7019558621eaf7044ae7f0fa
lyndsiWilliams <kcatgirl@gmail.com>
2021-01-31 22:56:36 -0600

    Lesson 19 complete

--- src/app.ts
+++ src/app.ts
@@ -1,88 +1,91 @@
-// // TS doesn't know if the anchor tag actually exists before compilation
-// // Trying to access this will result in an error unless you either:
-// // - Use an ! at the end
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-// // OR
-// // - Use conditional rendering
-// if (anchor){
-//     console.log(anchor.href);
-// };
+/*
+// TS doesn't know if the anchor tag actually exists before compilation
+// Trying to access this will result in an error unless you either:
+// - Use an ! at the end
+const anchor = document.querySelector('a')!;
+console.log(anchor.href);
+// OR
+// - Use conditional rendering
+if (anchor){
+    console.log(anchor.href);
+};
+*/
 
 import { Invoice } from "./classes/Invoice.js";
 import { ListTemplate } from "./classes/ListTemplate.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-// // ---------- Interfaces ----------
-// // Used to enforce a structure within classes/objects
-// // There is no interface to create an "IsPerson"
-// // - This is literally a filter to set structure
-// interface IsPerson {
-//   name: string;
-//   age: number;
+/*
+// ---------- Interfaces ----------
+// Used to enforce a structure within classes/objects
+// There is no interface to create an "IsPerson"
+// - This is literally a filter to set structure
+interface IsPerson {
+  name: string;
+  age: number;
 
-//   speak(a: string): void;
-//   spend(a: number): number;
-// };
+  speak(a: string): void;
+  spend(a: number): number;
+};
 
-// const me: IsPerson = {
-//   name: 'Lyndsi',
-//   age: 30,
+const me: IsPerson = {
+  name: 'Lyndsi',
+  age: 30,
 
-//   // These can change as long as they stay in structure
-//   speak(text: string): void {
-//     console.log(text);
-//   },
-//   spend(amount: number): number {
-//     console.log('I spent ', amount);
-//     return amount;
-//   }
-// };
+  // These can change as long as they stay in structure
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  }
+};
 
-// const greetPerson = (person: IsPerson) => {
-//   console.log('Hello ', person.name);
-// };
+const greetPerson = (person: IsPerson) => {
+  console.log('Hello ', person.name);
+};
 
-// greetPerson(me)
+greetPerson(me)
 
-// console.log(me)
+console.log(me)
 
-// // ---------- Modules ----------
-// // Only modern browsers support this
-// // Import the JS file, not TS
-// import { Invoice } from './classes/Invoice.js';
-// import { Payment } from './classes/Payment.js';
-// import { HasFormatter } from './interfaces/HasFormatter.js';
+// ---------- Modules ----------
+// Only modern browsers support this
+// Import the JS file, not TS
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// let documentOne: HasFormatter;
-// let documentTwo: HasFormatter;
+let documentOne: HasFormatter;
+let documentTwo: HasFormatter;
 
-// documentOne = new Invoice('Yoshi', 'Web work', 250);
-// documentTwo = new Payment('Mario', 'Plumbing work', 200);
+documentOne = new Invoice('Yoshi', 'Web work', 250);
+documentTwo = new Payment('Mario', 'Plumbing work', 200);
 
-// let docs: HasFormatter[] = [];
+let docs: HasFormatter[] = [];
 
-// docs.push(documentOne);
-// docs.push(documentTwo);
+docs.push(documentOne);
+docs.push(documentTwo);
 
-// console.log(docs);
+console.log(docs);
 
-// const invoiceOne = new Invoice('Mario', 'Work on the Mario website', 250);
-// const invoiceTwo = new Invoice('Luigi', 'Work on the Luigi website', 300);
+const invoiceOne = new Invoice('Mario', 'Work on the Mario website', 250);
+const invoiceTwo = new Invoice('Luigi', 'Work on the Luigi website', 300);
 
-// // This array can now only have Invoice types
-// let invoices: Invoice[] = [];
-// invoices.push(invoiceOne);
-// invoices.push(invoiceTwo);
+// This array can now only have Invoice types
+let invoices: Invoice[] = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
 
-// You can change these objects with dot notation
-// invoiceOne.client = 'Yoshi';
-// invoiceTwo.amount = 400;
+You can change these objects with dot notation
+invoiceOne.client = 'Yoshi';
+invoiceTwo.amount = 400;
 
-// invoices.forEach(inv => {
-//   console.log(inv.client, inv.amount, inv.format());
-// });
+invoices.forEach(inv => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 
 // ---------- Type Casting ----------
@@ -151,3 +154,26 @@ form.addEventListener('submit', (e: Event) => {
  };
 
  console.log(docThree, docFour);
+*/
+
+// ---------- Enums ----------
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+
+interface Resource<T> {
+  uid: number;
+  resourceType: ResourceType;
+  data: T;
+}
+
+const docOne: Resource<object> = {
+  uid: 1,
+  resourceType: ResourceType.BOOK,
+  data: { title: 'Name of the Wind' }
+}
+const docTwo: Resource<object> = {
+  uid: 10,
+  resourceType: ResourceType.PERSON,
+  data: { name: 'Yoshi' }
+}
+
+console.log(docOne, docTwo);
